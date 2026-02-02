1cef65653ff0a2695b56935103fc211420befeff
ItsKensann <114538139+ItsKensann@users.noreply.github.com>
2026-02-01 22:00:47 -0800

    added social links
    - social media icons on the bottom left of the screen that has links for linkedin, github and instagram

--- components/sections/ContactSection.tsx
+++ components/sections/ContactSection.tsx
@@ -5,32 +5,6 @@ import { color, motion } from "framer-motion";
 import { RetroWindow } from "@/components/RetroWindow";
 import { Mail, Send, BookUser } from "lucide-react";
 
-// TODO: add link to resume
-// name:
-// email:
-
-// lucide-react doesn' support brand specific icons due to legal restrications so manually download svg files
-const socialLinks = [
-  {
-    name: "GitHub",
-    icon: "",
-    href: "https://github.com/ItsKensann",
-    color: "#c4b5fd",
-  },
-  {
-    name: "LinkedIn",
-    icon: "",
-    href: "https://www.linkedin.com/in/kensanputra/",
-    color: "#93c5fd",
-  },
-  {
-    name: "Instagram",
-    icon: "",
-    href: "https://www.instagram.com/itskensan/",
-    color: "#93c5fd",
-  },
-];
-
 export function ContactSection() {
   return (
     <section
@@ -100,8 +74,8 @@ export function ContactSection() {
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         viewport={{ once: true }}
-        transition={{ delay: 0.6, duration: 0.5 }}
-        className="w-full max-w-4xl text-center mt-8"
+        transition={{ delay: 0.4, duration: 0.5 }}
+        className="w-full max-w-4xl text-center mt-16"
       >
         <div className="font-mono py-2 px-4 bg-[#f5f0ff] border-2 border-[#1a1a2e] shadow-[4px_4px_0px_0px_rgba(26,26,46,0.8)]">
           <p className="text-xs sm:text-sm text-[#4a4a6a]">
