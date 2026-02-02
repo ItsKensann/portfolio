757b9ae90bbac34f96bee28787f5affa050b673a
ItsKensann <114538139+ItsKensann@users.noreply.github.com>
2026-02-01 19:54:29 -0800

    typewriter component
    - typewriter component completed
    - import in hero and about me section and pass in text prop to have the typewriter animation displayed

---
+++ components/Typewriter.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+// interface for what to pass into the function as prop
+interface TypeWriterProps {
+  text: string;
+  delay?: number;
+  className?: string;
+}
+
+export function Typewriter({ text, delay = 50, className }: TypeWriterProps) {
+  const [displayText, setDisplayText] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const randomDelay = delay + Math.random() * 100;
+
+  useEffect(() => {
+    if (currentIndex < text.length) {
+      const timeout = setTimeout(() => {
+        setDisplayText((prev) => prev + text[currentIndex]);
+        setCurrentIndex((prev) => prev + 1);
+      }, randomDelay);
+      return () => clearInterval(timeout);
+    }
+  }, [currentIndex, delay, text]);
+
+  return (
+    <span>
+      {displayText}
+      {currentIndex < text.length && <span className="animate-pulse">|</span>}
+    </span>
+  );
+}
